a5197b1783039ed67a0148f0a1c2b45f8c5897d5
mkulupe <fkamushipu@gmail.com>
2019-11-05 14:38:46 -0800

    Added Mahangu

--- src/data/crops.js
+++ src/data/crops.js
@@ -11,10 +11,28 @@ export default{
             soilPreferences:''
 
          },
-         {
-             name: 'app',
-             scientificName: ''
-         }
+         { name: 'Pearl Millet/Mahangu', 
+            scientificName: 'Pennisetum glaucum',
+            shortDescription: 'Pearl millet is the major food cereal in the northern communinal areas and is popularly known as mahangu. Pearl millet is normally reserved for those areas where maize and sorghum fail to grow because of low rainfall or adverse soil factors. ',
+            image:'/src/assets',
+            sowingTime:'',
+            annualRainfall:'',
+            harvestTime:'',
+            sowingDepth:'',
+            soilPH:'',
+            soilPreferences:''
+        }
+        { name: '', 
+        scientificName: '',
+        shortDescription: '',
+        image:'',
+        sowingTime:'',
+        annualRainfall:'',
+        harvestTime:'',
+        sowingDepth:'',
+        soilPH:'',
+        soilPreferences:''
+    }
 
 ]
 }

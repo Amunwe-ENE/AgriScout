c90b99f8772da62e6e8c053e0c8cd02d5c51baf3
mkulupe <fkamushipu@gmail.com>
2019-11-06 11:46:41 -0800

    Groundnuts added

--- src/data/crops.js
+++ src/data/crops.js
@@ -25,7 +25,7 @@ export default{
         { name: 'Sorghum', 
         scientificName: 'Sorghum bicolor',
         shortDescription: 'Sorghum is a traditional staple food throughout the semi-arid areas of Africa. It is  a crop of multiple uses.',
-        image:'C:\Users\Hp user 2nd\Desktop\Vue Projects\AgriScout\src\assets\image\crops\'sorghum.jpg',
+        image:'\src\assets\image\crops\'sorghum.jpg',
         sowingTime:'October / November',
         annualRainfall:'300 - 400 mm ( highly drought resistant )',
         harvest:'Harvest at complete ripeness; thresh by beating with stick / thresher.',
@@ -36,7 +36,7 @@ export default{
     { name: 'Maize', 
             scientificName: 'Zea mays',
             shortDescription: 'It is an annual summer crop with a large number of cultivators including corn, popcorn etc. Maize is cross-pollinated. The kernel is a fruit with one seed and in mature stage consist of three main parts: the teets, the starchy endosperm and the embryo.',
-            image:'C:\Users\Hp user 2nd\Desktop\Vue Projects\AgriScout\src\assets\image\crops\maize.jpg',
+            image:'\src\assets\image\crops\maize.jpg',
             sowingTime:'Mid-December to mid-February',
             annualRainfall:'500-900ml',
             harvest:'Harvest as soon as maize is dry to avoid damage by rodents,termites, weevils and grain moths in the field',
@@ -49,13 +49,37 @@ export default{
          { name: 'Cowpeas', 
             scientificName: 'Vigna unguiculata',
             shortDescription: 'Cowpeas are able to fix nitrogen in their roots with the help of bacteria, therefore increasing soil fertility. It is an ideal crop for Namibia as toerates heat and relatively dry weather conditions',
-            image:'C:\Users\Hp user 2nd\Desktop\Vue Projects\AgriScout\src\assets\image\crops\cowpease.jpg',
+            image:'\src\assets\image\crops\cowpease.jpg',
             sowingTime:'Since cowpeas are cold-sensitive they should be planted early in the season, as soon as the first rains are expected',
-            annualRainfall:'500ml per year',
+            annualRainfall:'500mm per year',
             harvest:'for human consumptions, pods can be picked when they ripen i.e when the colour starts to change from green to purplish or cream',
             sowingDepth:'5-10cm in sandy soil, 3-7cm in heavy soils',
             soilPH:'Does not tolerate alkaline(pH >=8) or acid(ph <=4.5) soils',
             soilPreferences:'Sandy topsoil with 5-10% clay and sandy loam and underneath with 10-20% clay contents. Needs 1.2 m deep soils'
+
+         },
+         { name: 'Groundnuts', 
+            scientificName: 'Arachis hypogea',
+            shortDescription: 'Groundnuts are good source of protein and energy. Because it is legume, it will improve soil fertility when grown in rotation with other crops like maize or pearl millet.',
+            image:'\src\assets\image\crops\Groundnuts.jpg',
+            sowingTime:'Mid-November to mid-December in very wet soil.',
+            annualRainfall:'450 - 600 mm',
+            harvest:'Pods may be lifted with a spade or by shallow ploughing just underneath the pods. The whole plant is then pulled over a frame, covered with wire mesh. The pods fall through the mesh and should be picked up immediately and dried on a platform for two days after which they are placed in hessian bags for storage.',
+            sowingDepth:'5 - 7 cm',
+            soilPH:'Neutral to acid soils but not lower than pH of 5 is ideal.',
+            soilPreferences:'Light sandy, loamy soil with good stucture and a clay content of 8 - 10%,'
+
+         },
+         { name: '', 
+            scientificName: '',
+            shortDescription: '',
+            image:'',
+            sowingTime:'',
+            annualRainfall:'',
+            harvest:'',
+            sowingDepth:'',
+            soilPH:'',
+            soilPreferences:''
 
          }
 

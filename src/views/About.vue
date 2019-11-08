8f02a6e76b2959d62429daacdb68d93fd012d253
BrooksTheGuy <43580323+BrooksTheGuy@users.noreply.github.com>
2019-11-08 11:47:18 +0200

    Getting pics to work

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <weatherApi> 
+      
+    </weatherApi>
   </div>
 </template>
+
+<script>
+import weatherApi from '../components/weatherApi.vue'
+export default {
+component : {
+  weatherApi
+}
+  
+}
+</script>
